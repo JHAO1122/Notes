35b063804dd4e0f0abc19a5a3d9c152058db6aec
JHAO1122 <3230105412@zju.edu.cn>
2026-07-17 21:56:43 +0800

    Deployed d26abd2f with MkDocs version: 1.6.1

--- javascripts/starry_map.js
+++ javascripts/starry_map.js
@@ -45,6 +45,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
         { id: "SL", parentId: "Prob-Stat", label: isEn ? "Statistical Learning" : "统计学习", radius: 15, color: colors.probSub, collapsed: true },
 
+        { id: "Ad-Prob", parentId: "Prob-Stat", label: isEn ? "Advanced Probability" : "高等概率论", radius: 15, color: colors.probSub, collapsed: true },
+
+        { id: "AA", parentId: "Algebra", label: isEn ? "Abstract Algebra" : "抽象代数", radius: 15, color: colors.algebra, collapsed: true },
+
+        { id: "DM", parentId: "Geometry", label: isEn ? "Differential Manifolds" : "微分流形与黎曼几何", radius: 15, color: colors.geometry, collapsed: true },
+
+        { id: "Ad-Stat", parentId: "Prob-Stat", label: isEn ? "Advanced Mathematical Statistics" : "高等数理统计", radius: 15, color: colors.probSub, collapsed: true },
+
         // 泛函的内部章节
         { id: "FA-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "FA", radius: 8, color: colors.analysisLeaf, url: "Functional_Analysis/0Intro/" },
         { id: "FA-Ch1", label: isEn ? "1. Metric Spaces" : "1.度量空间", parentId: "FA", radius: 8, color: colors.analysisLeaf, url: "Functional_Analysis/1distance_space/" },
@@ -114,6 +122,14 @@ document.addEventListener("DOMContentLoaded", function() {
         { id: "RA-Ch10", label: isEn ? "10. Radon-Nikodym" : "10.Radon-Nikodym定理", parentId: "RA", radius: 8, color: colors.analysisSub, url: "Real_analysis/10_radon_nikodym/" },
         { id: "RA-Ch11", label: isEn ? "11. FTC in R" : "11.微积分基本定理", parentId: "RA", radius: 8, color: colors.analysisSub, url: "Real_analysis/11_differentiation_ir/" },
         { id: "RA-Ch12", label: isEn ? "12. Lp Spaces" : "12.L^p空间", parentId: "RA", radius: 8, color: colors.analysisSub, url: "Real_analysis/12_lp_spaces/" },
+        //高等概率论
+        { id: "Ad-Prob-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "Ad-Prob", radius: 8, color: colors.probSub, url: "Advanced_Prob/0Intro/" },
+        //高等数理统计
+        { id: "Ad-Stat-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "Ad-Stat", radius: 8, color: colors.probSub, url: "Advanced_Stat/0Intro/" },
+        //微分流形与黎曼几何
+        { id: "DM-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "DM", radius: 8, color: colors.geometry, url: "Differential_Manifolds/0Intro/" },
+        //抽象代数
+        { id: "AA-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "AA", radius: 8, color: colors.algebra, url: "Abstract_Algebra/0Intro/" },
 
         //统计学习
         { id: "SL-Intro", label: isEn ? "0. Intro" : "课程简介", parentId: "SL", radius: 8, color: colors.probSub, url: "Statistical_Learning/0Intro/" },
@@ -144,6 +160,11 @@ document.addEventListener("DOMContentLoaded", function() {
         { source: "Geometry", target: "Top" },
         { source: "Analysis", target: "RA" },
         { source: "Prob-Stat", target: "SL" },
+        { source: "Prob-Stat", target: "Ad-Prob" },
+        { source: "Algebra", target: "AA" },
+        { source: "Geometry", target: "DM" },
+        { source: "Prob-Stat", target: "Ad-Stat" },
+
 
         { source: "FA", target: "FA-Intro" }, { source: "FA", target: "FA-Ch1" }, { source: "FA", target: "FA-Ch2" }, { source: "FA", target: "FA-Ch3" },
         { source: "FA", target: "FA-Ch4" }, { source: "FA", target: "FA-Ch5" }, { source: "FA", target: "FA-Ch6" }, { source: "FA", target: "FA-Ch7" },
@@ -162,6 +183,11 @@ document.addEventListener("DOMContentLoaded", function() {
         { source: "RA", target: "RA-Ch4" }, { source: "RA", target: "RA-Ch5" }, { source: "RA", target: "RA-Ch6" }, { source: "RA", target: "RA-Ch7" },
         { source: "RA", target: "RA-Ch8" }, { source: "RA", target: "RA-Ch9" }, { source: "RA", target: "RA-Ch10" }, { source: "RA", target: "RA-Ch11" },
         { source: "RA", target: "RA-Ch12" },
+        { source: "Ad-Prob", target: "Ad-Prob-Intro" },
+        { source: "Ad-Stat", target: "Ad-Stat-Intro" },
+        { source: "DM", target: "DM-Intro" },
+        { source: "AA", target: "AA-Intro" },
+
         { source: "SL", target: "SL-Intro" }, { source: "SL", target: "SL-Ch1" }, { source: "SL", target: "SL-Ch2" }, { source: "SL", target: "SL-Ch3" },
         { source: "SL", target: "SL-Ch4" }, { source: "SL", target: "SL-Ch5" }, { source: "SL", target: "SL-Ch6" }, { source: "SL", target: "SL-Ch7" },
         { source: "SL", target: "SL-Ch8" }, { source: "SL", target: "SL-Ch9" }, { source: "SL", target: "SL-Ch10" },
